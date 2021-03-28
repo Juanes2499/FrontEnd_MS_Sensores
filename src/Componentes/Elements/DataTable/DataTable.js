047fd9d094fb83a5b91e75d5e2fa70492b4e1fa9
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2021-03-27 19:31:38 -0500

    Se modifica el elemento de datatable

--- src/Componentes/Elements/DataTable/DataTable.js
+++ src/Componentes/Elements/DataTable/DataTable.js
@@ -198,3 +198,88 @@ export const DataTableColAction = ({configuration, data, columns, buttonActions}
     )
 
 }
+
+
+export const DataTable = ({configuration, data, columns}) => {
+
+    const [page, setPage] = useState(1);
+    const [displayLength, setDisplayLength] = useState(5);
+
+    const handleChangePage = (dataKey) =>{
+
+        setPage(dataKey)  
+    }
+
+    const handleChangeLength = (dataKey) => {
+        setPage(1)
+        setDisplayLength(dataKey)          
+    }
+    
+    const getData = (data) => {
+        const datos = data.filter((v, i) => {
+            const start = displayLength * (page - 1);
+            const end = start + displayLength;
+            return i >= start && i < end;
+        });
+                
+        return datos
+    }
+
+    return (
+        <div style={configuration.styleMargin}>
+            <Table
+                width={configuration.width}
+                height={configuration.height}
+                data={getData(data)}
+                bordered={configuration.bordered}
+                cellBordered={configuration.cellBordered}
+                autoHeight={configuration.autoHeight}
+                style={configuration.style}
+            >
+                {
+                    columns.map((item, index) => {
+                        return(
+                            <Column key={index} width={item.width} align={item.align} fixed={item.fixed} resizable={configuration.resizable}>
+                                <HeaderCell 
+                                    style={configuration.headerStyle} 
+                                >
+                                    {item.text}
+                                </HeaderCell>
+                                <Cell 
+                                    dataKey={item.key} 
+                                    style={configuration.cellStyle}
+                                />
+                            </Column>
+                        )
+                    })
+                }     
+            </Table>
+            <Table.Pagination
+                lengthMenu={[
+                    {
+                        value: 5,
+                        label: 5
+                    },
+                    {
+                        value: 10,
+                        label: 10
+                    },
+                    {
+                        value: 15,
+                        label: 15
+                    },
+                    {
+                        value: 20,
+                        label: 20
+                    }
+                ]}
+                activePage={page}
+                displayLength={displayLength}
+                total={data.length}
+                onChangePage={handleChangePage}
+                onChangeLength={handleChangeLength}
+            />
+        </div>
+    )
+
+}
