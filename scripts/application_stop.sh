ac8458614df06bca8da2d3a2ed22ed64abbe4262
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2021-04-04 16:55:16 -0500

    CI/CD 16

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -2,8 +2,8 @@
 #Stopping existing node servers
 echo "Deteniendo servicios desplegados de ReactJs"
 
-if [ sudo lsof -t -i:3000 ]; then
-  sudo kill -9 $(sudo lsof -t -i:3000) 
-else
-  echo "Not working"
-fi
+# if [ sudo lsof -t -i:3000 ]; then
+#   sudo kill -9 $(sudo lsof -t -i:3000) 
+# else
+#   echo "Not working"
+# fi
